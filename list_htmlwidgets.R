2fa3fe31d0aa410e07ec0878d69d75041a8e9419
etiennebacher <etienne.bacher@protonmail.com>
2020-12-29 22:21:53 +0100

    init

---
+++ list_htmlwidgets.R
@@ -0,0 +1,29 @@
+library(yaml)
+library(magrittr)
+library(tibble)
+
+# get list of widgets and details
+download.file("https://raw.githubusercontent.com/hafen/htmlwidgetsgallery/gh-pages/_config.yml", "config.yaml")
+x <- yaml.load_file("config.yaml")
+
+r_name_and_repo <- lapply(x$widgets, function(y) {
+  paste0("[", y$name, "](", y$url, ")")
+})
+
+js_name_and_repo <- lapply(x$widgets, function(y) {
+  if (is.null(y$jslibs)) {
+    y$jslibs <- NA
+  }
+  y$jslibs %>%
+    gsub("\\),\\[", "\\), \\[", .)
+})
+
+
+from_htmlwidgets_gallery <- data.frame(
+  "r_package" = unlist(r_name_and_repo),
+  "js_library" = unlist(js_name_and_repo)
+)
+
+other_packages <- read.csv("other_packages.csv", stringsAsFactors = FALSE)
+
+output <- rbind(from_htmlwidgets_gallery, other_packages)
